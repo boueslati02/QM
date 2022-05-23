71d04da01e4f47d289f5508b43e281526d4bd490
Bilel OUESLATI <boueslati-ext@ponant.com>
2022-05-23 15:04:44 +0200

    Envoyer Emaill avec la validation de QM

---
+++ Ponant.Medical.Shore/Controllers/EmailController.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Ponant.Medical.Data.Shore;
+using Ponant.Medical.Data.Shore.Models;
+using Ponant.Medical.Shore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ponant.Medical.Shore.Controllers
+{
+    public class EmailController : BaseController
+    {
+        private SendEmail _sendEmail;
+        public EmailController()
+        {
+            _sendEmail = new SendEmail(_shoreEntities);
+        }
+        // GET: Email
+        public ActionResult ValidQM()
+        {
+            string token = HttpContext.Request.QueryString["token"];
+            string DecryptToken = _sendEmail.DecryptString(token);
+            UserToken DescryptUser = JsonConvert.DeserializeObject<UserToken>(DecryptToken);
+            Cruise cruise = _shoreEntities.Cruise.Find(6);
+            if(cruise == null)
+            {
+                ViewBag.Message = "The cruise is cancelled";
+                return View("~/Views/ConfirmQM/CruiseCanceled.cshtml");
+            }else
+            {
+                int result = DateTime.Compare(cruise.SailingDate, DateTime.Now);
+                if (result < 0)
+                {
+                    ViewBag.Message = "Cruise already passed";
+                    return View("~/Views/ConfirmQM/CruiseCanceled.cshtml");
+                }
+                else
+                {
+                    Passenger passenger = _shoreEntities.Passenger.Where(p => p.Number.ToString() == DescryptUser.PassengerNo).FirstOrDefault();
+                    if (passenger.IdAdvice == 44)
+                    {
+                        ViewBag.Message = "You have already validated your medical questionnaire";
+                        return View("~/Views/ConfirmQM/CruiseCanceled.cshtml");
+                    }
+                    else
+                    {
+                        ViewBag.Token = token.Replace(" ", "+");
+                        ViewBag.DescryptUser = DescryptUser;
+                        ViewBag.Language = _shoreEntities.Language.Where(l => l.Id == 46).FirstOrDefault();
+                        return View("~/Views/ConfirmQM/ConfirmQM.cshtml");
+                    }
+                }
+            }
+            
+            
+            
+        }
+        [HttpPost]
+        public JsonResult ChangeStatusPassager()
+        {
+            string token = Request.Form["Token"];
+            string ConfirmQM = Request.Form["ConfirmQM"];
+            string DecryptToken = _sendEmail.DecryptString(token);
+            UserToken DescryptUser = JsonConvert.DeserializeObject<UserToken>(DecryptToken);
+            Passenger passenger = _shoreEntities.Passenger.Where(p => p.Number.ToString() == DescryptUser.PassengerNo).FirstOrDefault();
+            _sendEmail.ValidQM(passenger.Id);
+            return Json(true);
+        }
+
+
+         
+    }
+}
---
+++ Ponant.Medical.Shore/Models/SendEmail.cs
@@ -0,0 +1,152 @@
+﻿using Newtonsoft.Json;
+using Ponant.Medical.Common;
+using Ponant.Medical.Data;
+using Ponant.Medical.Data.Shore;
+using Ponant.Medical.Data.Shore.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Ponant.Medical.Shore.Models
+{
+    public class SendEmail : SharedClass
+    {
+        private readonly IShoreEntities _shoreEntities;
+        public SendEmail(IShoreEntities shoreEntities) : base(shoreEntities)
+        {
+            _shoreEntities = shoreEntities;
+        }
+        public Survey GetSurveyById(int Id)
+        {
+            return _shoreEntities.Survey.Where(s => s.Id == Id).FirstOrDefault();
+        }
+        public string EncryptString(string plainText)
+        {
+            string key = "b14ca5898a4e4133bbce2ea2315a1916";
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+        public string DecryptString(string cipherText)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            string key = "b14ca5898a4e4133bbce2ea2315a1916";
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+        public void SendHtmlMail(UserToken userToken)
+        {
+            try
+            {
+                string SerializeuserToken = JsonConvert.SerializeObject(userToken);
+                string TokenGenerated = EncryptString(SerializeuserToken);
+                MailMessage mail = new MailMessage();
+                string body = string.Empty;
+                Lov lov = _shoreEntities.Lov.Where(l => l.Name.Contains(userToken.Language)).FirstOrDefault();
+                string BouttonText = "Confirm Your Medical Question";
+                string SujectText = "Confirm Medical Question";
+                if (userToken.Language.ToUpper().Contains("FRANC"))
+                {
+                    BouttonText = "Confirmer Votre Questionnnaire médical";
+                    SujectText = "Confirmation Questionnaire médicale";
+                }else if (userToken.Language.ToUpper().Contains("ALLEMA"))
+                {
+                    BouttonText = "Ihren medizinischen Fragebogen bestätigen";
+                    SujectText = "Bestätigung Medizinischer Fragebogen";
+                }
+                string content = _shoreEntities.Language.Where(l => l.IdLanguage == lov.Id && l.IdSurvey == 11).FirstOrDefault().EmailFormat;
+                if (String.IsNullOrEmpty(content))
+                {
+                    body = _shoreEntities.Language.Where(l => l.IsDefault == true).FirstOrDefault().EmailFormat;
+                }
+                else
+                {
+                    body = content;
+                }
+                body = body.Replace("{FirstName}", userToken.FirstName);
+                body = body.Replace("{LastName}", userToken.LastName);
+                body = body.Replace("{CruiseNumber}", userToken.CruiseNumber);
+                var request = HttpContext.Current.Request;
+                string baseUrl = request.Url.Scheme + "://" + request.Url.Authority;
+                body = body + "<br><br><br><a href='" + baseUrl + "/Email/ValidQM?token=" + TokenGenerated + "' title='Titre' style='padding: 10px 15px;border-radius: 100px;border-style: solid;border-width: 1px;color: #fff;text-align: center;font-weight: 800;border-color: #26afc2;background-color: #26afc2;'>"+ BouttonText + "</a> ";
+                mail.To.Add(userToken.Email);
+                mail.From = new MailAddress("boueslati-ext@ponant.com");
+                mail.Subject = SujectText;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.outlook.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("boueslati-ext@ponant.com", "6azerty*");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public bool ValidQM(int IdPassager)
+        {
+            
+            try
+            {
+                Passenger passenger = _shoreEntities.Passenger.Find(IdPassager);
+                passenger.IdAdvice = Constants.ADVICE_FAVORABLE_OPINION;
+                _shoreEntities.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+            
+        }
+    }
+}
